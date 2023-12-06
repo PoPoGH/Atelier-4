d9ff3010b9a9f019f89d908465d8a7666ade5784
PoPo <76515395+PoPoGH@users.noreply.github.com>
2023-12-06 09:42:39 +0100

    Update make_html.sh

--- make_html.sh
+++ make_html.sh
@@ -2,5 +2,18 @@
 
 JARFILE="${1:-biblio.jar}"
 
+# Check if the JAR file exists
+if [ ! -f "$JARFILE" ]; then
+    echo "Error: JAR file $JARFILE does not exist."
+    exit 1
+fi
+
+# Check if the output directory exists and is writable
+if [ ! -w wwwroot ]; then
+    echo "Error: The wwwroot directory does not exist or is not writable."
+    exit 1
+fi
+
+# Execute the Java application for each input file
 cat data/_liste.txt data/fg67hIOrf.txt | java -cp $JARFILE epsi.biblio.App > wwwroot/fg67hIOrf.html
 cat data/_liste.txt data/rt25TYuy0.txt | java -cp $JARFILE epsi.biblio.App > wwwroot/rt25TYuy0.html
